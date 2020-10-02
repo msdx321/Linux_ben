64c956a5d1c870d0b47f43213781a382fcd3b391
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-10-03 00:47:03 +0800

    update parser

--- Linux_client/Linux_expr/parse.py
+++ Linux_client/Linux_expr/parse.py
@@ -3,41 +3,52 @@ import sys
 import numpy as np
 
 #file = str(sys.argv[1])
-s_file = int(sys.argv[1])
-nb = int(sys.argv[2])
-res_avg = 0
-tail = 0
-tot = []
-length = 0
-res_min = float("inf")
-res_max = 0
-tot_sent = 0
+s_file = 0
+nb = 0
+num = 0
 
-for c in range(nb):
-    idx = s_file + c
-    filename = "logs/mcb_"+str(idx)
-    print filename
-    with open(filename, "r") as f:
-        for line in f:
-            if line.startswith("RTT:"):
-                temp = int(line.split()[1].strip())
-                res_min = min(res_min, temp);
-                res_max = max(res_max, temp);
-                res_avg += temp
-	        tot.append(temp)
-	        length += 1
-            elif line.startswith("Requests sent"):
-                temp = int(line.split(None)[-1].strip())
-                tot_sent += temp
+def parse_file(head, number):
+    res_avg = 0
+    tail = 0
+    tot = []
+    length = 0
+    res_min = float("inf")
+    res_max = 0
+    tot_sent = 0
 
-tot.sort()
-tail = int(length*0.99)
+    for c in range(number):
+        idx = head + c
+        filename = "logs/mcb_"+str(idx)
+        with open(filename, "r") as f:
+            for line in f:
+                if line.startswith("RTT:"):
+                    temp = int(line.split()[1].strip())
+                    res_min = min(res_min, temp);
+                    res_max = max(res_max, temp);
+                    res_avg += temp
+	            tot.append(temp)
+	            length += 1
+                elif line.startswith("Requests sent"):
+                    temp = int(line.split(None)[-1].strip())
+                    tot_sent += temp
 
-print "parsed file:                {} - {}".format("mcb_"+str(s_file), "mcb_"+str(s_file+nb))
-print "tail latecny:               {} us".format(tot[tail])
-print "avgerage latency:           {} us".format(res_avg/length)
-print "MIN latency:                {} us".format(res_min)
-print "MAX latency:                {} us".format(res_max)
-print "number of RTTs measured:    {}".format(length)
-print "number of requests dropped: {}".format(tot_sent-length)
+    tot.sort()
+    tail = int(length*0.99)
 
+    print "[hi]parsed file:                {} - {}".format("mcb_"+str(s_file), "mcb_"+str(s_file+nb))
+    print "[hi]tail latecny:               {} us".format(tot[tail])
+    print "[hi]avgerage latency:           {} us".format(res_avg/length)
+    print "[hi]MIN latency:                {} us".format(res_min)
+    print "[hi]MAX latency:                {} us".format(res_max)
+    print "[hi]number of RTTs measured:    {}".format(length)
+    print "[hi]number of requests dropped: {}".format(tot_sent-length)
+    print "-----------------------------------------------------------"
+
+if __name__ == "__main__":
+    s_file = 11211
+    nb = int(sys.argv[1])
+    num = int(sys.argv[2])
+    print "hi flow"
+    parse_file(s_file, nb)
+    print "lo_flow"
+    parse_file(s_file+nb, num-nb)
