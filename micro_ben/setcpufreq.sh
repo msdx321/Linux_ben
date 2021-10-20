4c13903005799c43356480b512f0df6679181148
msdx321 <msdx321@gmail.com>
2021-10-20 01:42:45 -0400

    Clean up
    Signed-off-by: msdx321 <msdx321@gmail.com>

--- micro_ben/setcpufreq.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
-for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq; do echo "2700000" > $file; done
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
